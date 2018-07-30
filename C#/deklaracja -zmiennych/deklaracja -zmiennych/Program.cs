489757c2f0d38f3abaea67ee7a1df5ffd49c1e42
Lenovo\Admin <jarkus36@gmail.com>
2018-07-30 20:23:00 +0200

    deklaracja - zmiennych

---
+++ C#/deklaracja -zmiennych/deklaracja -zmiennych/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace deklaracja__zmiennych
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Składnia deklaracji zmiennych:
+            //Typ Nazwa [=wartość];
+
+            int x, y, z = 23;
+            x = 10;
+            Console.WriteLine(x);
+            Console.ReadKey();
+
+        }
+    }
+}

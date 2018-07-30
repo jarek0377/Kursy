bb0e129965439b2e9f6bbcdbd38c0d05ca741f87
Lenovo\Admin <jarkus36@gmail.com>
2018-07-30 22:30:01 +0200

    wyrazenia algebraiczne 03/01

---
+++ C#/wyrazenia algebraiczne  03-01/wyrazenia algebraiczne  03-01/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wyrazenia_algebraiczne__03_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Przykładowe wyrażenia: y=2ab-4 w języku c# zapiszemy:
+            int a = 2, b = 3, y;
+            y = 2 * a * b - 4;
+            Console.WriteLine(y);
+            Console.ReadKey();
+
+
+        }
+    }
+

bb41803bf8a7536248b6f25ee6341e5f57048f0a
Lenovo\Admin <jarkus36@gmail.com>
2018-08-01 15:05:09 +0200

    03/02-modyfikacja-typow-dla -lieralow

---
+++ C#/wyrazenia algebraiczne 2/wyrazenia algebraiczne 2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wyrazenia_algebraiczne_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int x=10;
+            //double z=2.5;
+            //double y=1d/2;
+            //Console.WriteLine(y);
+
+            //Przeliczanie temperatury ze skali Fahrenheita na Celcjusza
+            double F, C;
+            Console.WriteLine("Podaj temp.w stopniach Fahrenheita");
+            F = double.Parse(Console.ReadLine());
+            C = 5.0 / 9 * (F - 32);
+            Console.WriteLine(C);
+            Console.ReadKey();
+
+
+
+          
+
+        }
+
+    
+        }
+    }

fed4b2a52d71f07e98c5410c65ccbd69a1bed953
Lenovo\Admin <jarkus36@gmail.com>
2018-08-01 18:47:48 +0200

    moj pierwszy program

---
+++ C#/przelicz walute/przelicz walute/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace przelicz_walute
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int x=10;
+            //double z=2.5;
+            //double y=1d/2;
+            //Console.WriteLine(y);
+
+            //Przeliczanie kursu walutowego 
+            double F, C;
+            Console.WriteLine("Podaj kwote Euro");
+            F = double.Parse(Console.ReadLine());
+            C = F * 4.263;
+            Console.WriteLine(C);
+            Console.WriteLine(" wartosc w zlotych ,Milego Dnia");
+            Console.ReadKey();
+
+
+
+        }
+    }
+
+    
+    
+}

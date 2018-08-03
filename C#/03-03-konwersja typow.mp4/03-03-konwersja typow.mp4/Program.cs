81923d2af37ddd8af91e121e1d2391d0c57edc9f
Lenovo\Admin <jarkus36@gmail.com>
2018-08-03 20:33:27 +0200

    03-03 konwersja typow

---
+++ C#/03-03-konwersja typow.mp4/03-03-konwersja typow.mp4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_03_konwersja_typow.mp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+       
+        {
+            const int liczbaMiesiecy = 12;
+            int liczbaSamochodow;
+            double sredniaSprzedaz;
+            Console.WriteLine("Podaj roczną sprzedaż samochodów : ");
+            liczbaSamochodow = int.Parse(Console.ReadLine());
+            sredniaSprzedaz = (double)liczbaSamochodow / (double)liczbaMiesiecy;
+            Console.WriteLine(sredniaSprzedaz);
+            Console.ReadKey();
+
+
+            
+        }
+    }
+}

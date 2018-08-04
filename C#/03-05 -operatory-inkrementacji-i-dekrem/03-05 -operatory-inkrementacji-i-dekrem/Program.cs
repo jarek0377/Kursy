9f4fd282e2d7374ef190faba945a3454cb2ac1d0
Lenovo\Admin <jarkus36@gmail.com>
2018-08-04 10:14:10 +0200

    03-05- operatory

---
+++ C#/03-05 -operatory-inkrementacji-i-dekrem/03-05 -operatory-inkrementacji-i-dekrem/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_05__operatory_inkrementacji_i_dekrem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0, wynik;
+            wynik = ++x * 3;
+            Console.WriteLine(x);
+            Console.WriteLine(wynik);
+            Console.ReadKey();
+
+        }
+    }
+}

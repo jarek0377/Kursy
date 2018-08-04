b95ef357e53d2f51019ba1082748f0bf00e6adb1
Lenovo\Admin <jarkus36@gmail.com>
2018-08-04 12:55:36 +0200

    obliczanie ilosci drzewek do obsadzenia

---
+++ C#/ogrodnictwo ilosc drzewek do obsadzenia/ogrodnictwo ilosc drzewek do obsadzenia/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ogrodnictwo_ilosc_drzewek_do_obsadzenia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double ilosc;
+            double dlugoscodcinka, odleglosc;
+            Console.WriteLine("Podaj dlugość odcinka do obsadzenia(w metrach)");
+            dlugoscodcinka= double .Parse(Console.ReadLine());
+            Console.WriteLine("Podaj odleglość między drzewkami(w cm)");
+            odleglosc=double.Parse(Console.ReadLine());
+            ilosc = dlugoscodcinka  / odleglosc * 100;
+            Console.WriteLine("Zakup :") ;
+            Console.WriteLine(ilosc);
+            Console.ReadKey();
+
+
+            
+        }
+    }       
+
+}
+
+
+

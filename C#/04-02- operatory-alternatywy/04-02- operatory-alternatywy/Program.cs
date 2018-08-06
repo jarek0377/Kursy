843d08daa20afbc1b7e2e2466d185034b203b1cc
Lenovo\Admin <jarkus36@gmail.com>
2018-08-06 09:01:26 +0200

    04-02 operatory-alternatywy

---
+++ C#/04-02- operatory-alternatywy/04-02- operatory-alternatywy/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_02__operatory_alternatywy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x= 3;
+            bool wynik;
+            wynik = (x >= 2| x++ >= 4);
+            Console.WriteLine(wynik);
+            Console.WriteLine(x);
+            Console.ReadKey();
+
+        }
+    }
+}

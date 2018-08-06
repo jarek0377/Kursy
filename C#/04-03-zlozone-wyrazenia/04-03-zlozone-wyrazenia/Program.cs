75705d971742f720a23a64efe12e8350879a33f3
Lenovo\Admin <jarkus36@gmail.com>
2018-08-06 13:57:06 +0200

    04-03 -zlozone-wyrazenia

---
+++ C#/04-03-zlozone-wyrazenia/04-03-zlozone-wyrazenia/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_03_zlozone_wyrazenia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 1, y = 2;
+            bool wynik;
+            wynik = ((x == 1 || x == 3 || x == 5) && y <= 1);
+            Console.WriteLine(!wynik);
+            Console.ReadKey();
+
+        }
+    }
+}

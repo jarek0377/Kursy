55a9e959aa99ffd96520f4862885c15f752208ab
Lenovo\Admin <jarkus36@gmail.com>
2018-08-10 16:25:19 +0200

    05=01 -operator

---
+++ C#/05-01-operato-konkatenacji/05-01-operato-konkatenacji/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_01_operato_konkatenacji
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj imię");
+            string tekst = Console.ReadLine();
+            tekst = "Cześć"  + tekst;
+            Console.WriteLine(tekst);
+            Console.WriteLine(tekst.Length);
+            Console.ReadKey();
+        }
+    }
+}

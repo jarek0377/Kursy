dd55e2e08729b2e22c596991764e83298a58049d
Lenovo\Admin <jarkus36@gmail.com>
2018-08-12 07:38:39 +0200

    05-02-znaki-specjalne

---
+++ C#/05-02 -znaki -specjalne/05-02 -znaki -specjalne/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_02__znaki__specjalne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Linia1\nLinia2");
+            Console.WriteLine("Kolumna1\tKolumna2");
+            Console.WriteLine(@"Kolumna1\tKolumna2");
+            Console.WriteLine("C:\\Windows\\Temp");
+            Console.WriteLine("Tytuł filmu: \"Dom\"");
+
+
+        }
+
+
+    }
+}
